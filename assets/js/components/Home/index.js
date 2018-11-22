853ec286eeaffe2c79e4c093943213c196c14e2d
Anh Mai <theanht1k20@gmail.com>
2018-11-22 08:18:33 +0700

    UI config

--- assets/js/components/Home/index.js
+++ assets/js/components/Home/index.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
-import Game from '../../game/game';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+
 
 class Home extends Component {
   componentDidMount() {
-    const game = new Game('canvas');
-    game.start();
   }
 
   render() {
     return (
-      <div className="row">
-        <div className="col">
-          <h1>Snake Vim Trainer</h1>
-          <h2>Hone your vim navigation skillz</h2>
-          <div className="game">
-            <canvas id="canvas" width="500" height="500"></canvas><br />
-            <p>Score: <span id="score"></span></p>
-          </div>
-        </div>
+      <div className='login-form'>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='center'>
+              Log-in to your account
+            </Header>
+            <Form size='large'>
+              <Segment stacked>
+                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                />
 
-        <div className="col">
-          <h2>Instructions</h2>
-          <p>Make your Vim snake eat the food to increase your score.</p>
-          <p>You can only eat food while INSERT mode is on.</p>
-          <p>You cannot change direction while INSERT mode is on</p>
-          <p>Controls:</p>
-          <ul>
-            <li>Left: h</li>
-            <li>Down: j</li>
-            <li>Up: k</li>
-            <li>Right: l</li>
-            <li>Insert mode: i</li>
-            <li>Normal mode: esc</li>
-          </ul>
-
-        </div>
+                <Button color='teal' fluid size='large'>
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              New to us? <a href='#'>Sign Up</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,13 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 
 import store from './store';
 import App from './components/App';
-import css from '../css/app.css';
 
 // import socket from './socket'
 import 'phoenix_html';
+import '../css/app.css';
 
 ReactDOM.render(
   <Provider store={store}>
