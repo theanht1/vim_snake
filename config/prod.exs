5b4ecbe2df68899599dfe1ce92a2f0ee18774494
Anh Mai <theanht1k20@gmail.com>
2020-06-21 19:59:00 +0700

    chore: Update config for production

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :vim_snake, VimSnakeWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "safe-harbor-46637.herokuapp.com", port: 443],
+  url: [scheme: System.get_env("SCHEME") || "https", host: System.get_env("HOST"), port: System.get_env("SCHEME_PORT") || 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
